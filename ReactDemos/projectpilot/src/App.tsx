2d71d52c1491169713f08d504aa9a943cf774996
saketkarnik79 <saket@optimistikinfo.com>
2025-09-26 09:25:45 +0530

    Day14

---
+++ ReactDemos/projectpilot/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        {/* <h1>
+          Ready for React Development
+        </h1> */}
+        {/* <blockquote cite="Benjamin Franklin">
+          Tell me & I forget, teach me & I may remember, involve me & I learn.
+        </blockquote> */}
+        <div className='container'>
+          <ProjectsPage/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
